e9d72d99270bd75d07ac7e1177a0210f349054bb
nikechervon <kolyachervon@yandex.ru>
2021-03-17 20:03:11 +0500

    Initialization commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/style.css">
+    <title>Start page</title>
+</head>
+<body>
+
+    <h1>Start page:</h1>
+
+    <a href="/task1" class="start-page__link">
+        <button>Задание 1: Високосный год.</button>
+    </a>
+
+    <a href="/task2" class="start-page__link">
+        <button>Задание 2: Префикс.</button>
+    </a>
+
+    <a href="/task3" class="start-page__link">
+        <button>Задание 3: Хипстеры и смузи.</button>
+    </a>
+
+</body>
+</html>
