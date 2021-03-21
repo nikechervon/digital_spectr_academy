08548c97e324d586b883c0c0322654d8296bd890
nikechervon <kolyachervon@yandex.ru>
2021-03-21 15:20:32 +0500

    Обновил файл запуска build.sh

--- backend/bash/build.sh
+++ backend/bash/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd digital_spectr_academy/backend/task1 && composer install
+cd digital-spectr.loc/backend/task1 && composer install
 cd ../task2 && composer install
 cd ../task3 && composer install
